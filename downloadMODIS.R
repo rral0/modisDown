52258353e79235ff780386de1ccd63765c2cd2c8
rral <albert.physik@gmail.com>
2021-03-11 18:07:15 -0500

    change from hdf to tiff format, conversion factor and data download were also added

---
+++ downloadMODIS.R
@@ -0,0 +1,26 @@
+# loading all dependency packages
+pkgs <- c("terra", "luna", "raster")
+lapply(pkgs, require, character.only = TRUE)
+
+# product MODIS to download
+# product <- "MOD09A1"
+product <- "MOD13Q1"
+
+# loading shapefile of area of interest (aoi)
+area <- shapefile("data/shape/Cultivos_Mango.shp")
+# plot(area)
+ext  <- extent(area)
+
+data_path <- "data/"
+
+source("credentials.R")
+
+# Setting start and end of download time
+start <- "2021-03-01"
+end   <- "2021-03-30"
+
+# downloading MODIS data using the above parameters
+f <- getModis(product = product, start_date = start,
+              end_date = end, aoi = ext, download = TRUE,
+              path = data_path, username = user,
+              password = passwd, overwrite = TRUE)
---
+++ hdf2tif.R
@@ -0,0 +1,27 @@
+# changing the working directory (need rstudioapi package)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# rstudioapi: only under RStudio IDE
+cat("Working directory: ", getwd())
+
+# loading all dependency packages
+pkgs <- c("terra", "raster", "stringr")
+lapply(pkgs, require, character.only = TRUE)
+
+# All *.hdf files are in D:/Ci_data/HDF/ directory
+input_path  <- "D:/Ci_data/HDF/"
+output_path <- "D:/Ci_data/TIF/"
+files_name  <- list.files(input_path, pattern = "*.hdf")
+name_save   <- gsub(".hdf", ".tif", files_name)
+
+full_path  <- paste0(input_path, files_name)
+
+# extract and write only the NDVI and EVI layers
+# index <- c(1:2) # NDVI, EVI
+index <- 1 # NDVI
+for (i in seq_len(length(full_path))) {
+  cat("Proceso en: ", i, "\n")
+  data_tmp  <- rast(full_path[i])[[index]]/100000000
+  name_file <- paste0(output_path, name_save[i])
+  writeRaster(data_tmp, filename = name_file, format = "GTiff", 
+              overwrite = TRUE)
+}
