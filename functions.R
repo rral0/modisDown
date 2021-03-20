82e20395a52ffa1e4f9fa227bcbc5f1dc6fa4bd2
rral <albert.physik@gmail.com>
2021-03-20 18:36:33 -0500

    added the function to extract the months considering whether it is a leap year or not

--- functions.R
+++ functions.R
@@ -6,15 +6,44 @@ subsi <- function(x, n=1,m=0){
   substr(x, nchar(x)-n-m+1, nchar(x)-m)
 }
 
-year_extraction <- function(x) {
-  posi <- unlist(str_locate_all(pattern ='A20', x[1])) + 1
-  anio <- substr(x, posi[1], (posi[2] + 1))
+year_extraction <- function(x, i) {
+  posi <- stringr::str_detect(x, paste0("MOD13Q1.A", i))
+  anio <- which(posi == TRUE)
   return(anio)
 }
 
 band_interest <- function(x, patron) {
-  posi <- str_locate_all(pattern = patron, x)
+  posi <- stringr::str_locate_all(pattern = patron, x)
   posi_find <- unlist(lapply(posi, length))
   posi_tmp <- which(posi_find == 2)
   return(posi_tmp)
+}
+
+idx_month <- function(monty, i){
+  mm <- switch(monty,
+               ene = c(1, 2),
+               feb = c(3, 4),
+               mar = c(5, 6),
+               abr = c(7, 8),
+               may = c(9, 10),
+               jun = c(11, 12),
+               jul = c(13, 14),
+               ago = c(15, 16),
+               set = c(17, 18),
+               # ---------------------------------
+               # TODO: improve this syntax
+               oct = if (lubridate::leap_year(i)){
+                 oct <- c(19, 20)
+               }else{
+                 oct <- 19
+               },
+               nov = if (lubridate::leap_year(i)){
+                 oct <- 21
+               }else{
+                 oct <- c(20, 21)
+               },
+               # ---------------------------------
+               dic <- c(22, 23)
+  )
+  return(mm)
 }
